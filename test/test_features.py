a0ee95789b17707a98ea342654c1501a9924d8ae
kugurst <ma2799@gmail.com>
2019-02-26 02:01:10 -0600

    Full run of all songs

--- test/test_features.py
+++ test/test_features.py
@@ -12,6 +12,30 @@ def test_blah():
     print(len(songs))
 
 
+def test_iterate_over_all_features():
+        good_songs, bad_songs = [], []
+        good_collection = SongInfoDatabase.db.collection(database.DB_GOOD_SONGS)
+        bad_collection = SongInfoDatabase.db.collection(database.DB_BAD_SONGS)
+        total_len = len(good_collection) + len(bad_collection)
+        for collection, song_bin, is_good_song in [(good_collection, good_songs, True),
+                                                   (bad_collection, bad_songs, False)]:
+            for idx in range(len(collection)):
+                record = collection.fetch(idx)
+                song_sample_id = SongInfoDatabase.SongInfoODBC.SONG_SAMPLES_UNQLITE_ID.get_value(record)
+                if song_sample_id >= total_len - 1:
+                    continue
+
+                if is_good_song:
+                    good_songs.append((song_sample_id, 1))
+                else:
+                    bad_songs.append((song_sample_id, 0))
+
+        songs, conn = SongSampleZODBDatabase.get_songs(True)
+        for song_id, is_good_song in good_songs + bad_songs:
+            song = songs.get(song_id)
+            print(song.get_samples_left()[0].shape)
+
+
 def test_compute_features():
     gsrc = SongInfoDatabase.db.collection(database.DB_GOOD_SONGS)
     song_index = random.randint(0, len(gsrc) - 1)
