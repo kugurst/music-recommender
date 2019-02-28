56e8e18a29e665b804e53c209d0de9890ef8e3df
kugurst <ma2799@gmail.com>
2019-02-28 11:55:56 -0600

    Added a function to pre-compute features

--- test/test_features.py
+++ test/test_features.py
@@ -1,6 +1,17 @@
+import pyarrow
+import copy
+import json
+import marshal
+import multiprocessing
+import numpy as np
+import os
+import pickle
 import random
 import tempfile
 import time
+import ujson
+
+import transaction
 
 from file_store import database
 from file_store.database import *
@@ -51,3 +62,61 @@ def test_compute_features():
 def test_generate_audio_sample():
     gsrc = SongInfoDatabase.db.collection(database.DB_GOOD_SONGS)
     generate_audio_sample(random.randint(0, len(gsrc) - 1), delete_on_exit=False)
+
+
+def test_load_samples(should_compute_features=True):
+    songs, _ = SongSampleZODBDatabase.get_songs(True)
+    queues = np.array_split(list(songs.keys()), multiprocessing.cpu_count())
+    transaction.abort()
+    SongSampleZODBDatabase.close_db()
+    del songs
+
+    with multiprocessing.Manager() as manager:
+        processes = []
+        for idx in range(multiprocessing.cpu_count()):
+            p = multiprocessing.Process(target=_load_samples, args=(set(queues[idx]), should_compute_features))
+            processes.append(p)
+            p.start()
+
+        for idx in range(multiprocessing.cpu_count()):
+            p = processes[idx]
+            p.join()
+
+
+def _load_samples(song_queue, should_compute_features=False):
+    songs, _ = SongSampleZODBDatabase.get_songs(True)
+    for song_idx in songs:
+        if song_idx not in song_queue:
+            continue
+
+        song_record = songs[song_idx]
+        if not should_compute_features:
+            song_record_copy = SongSamplesPickled(song_hash=song_record.song_hash, info_id=song_record.info_id,
+                                                  is_good_song=song_record.is_good_song,
+                                                  samples_left=song_record.get_samples_left(),
+                                                  samples_right=song_record.get_samples_right(),
+                                                  samples_indexes=song_record.samples_indexes)
+            song_record_copy = copy.deepcopy(song_record_copy)
+            with open("/capstone/tmp/sample_records/{}.pyarrow".format(song_idx), 'wb+') as f:
+                f.write(pyarrow.serialize(song_record_copy.tolist()).to_buffer())
+                # json.dump(song_record_copy.todict(), f)  # , protocol=pickle.HIGHEST_PROTOCOL)
+            print("Loaded song index [{}]".format(song_idx))
+        else:
+            to_pickle = []
+            to_pickle_small = []
+            for sample_idx in range(len(song_record.samples_indexes)):
+                features = compute_features(song_record, sample_idx, song_index_is_record=True)
+                features.song_record_id = song_idx
+                to_pickle.append(features.todict())
+                to_pickle_small.append(features.tosmalldict())
+
+            with open("/capstone/tmp_ramfs/features/{}.pyarrow".format(song_idx), 'wb+') as f:
+                f.write(pyarrow.serialize(to_pickle).to_buffer())
+                # json.dump(song_record_copy.todict(), f)  # , protocol=pickle.HIGHEST_PROTOCOL)
+            with open("/capstone/tmp_ramfs/features/{}_small.pyarrow".format(song_idx), 'wb+') as f:
+                f.write(pyarrow.serialize(to_pickle_small).to_buffer())
+                # json.dump(song_record_copy.todict(), f)  # , protocol=pickle.HIGHEST_PROTOCOL)
+            print("Computed features for song index [{}]".format(song_idx))
+
+        if song_idx % 15 == 14:
+            transaction.abort()
